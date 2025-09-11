5affab1b3ce9ca65a4b89f8c75f95f39f02a8c4b
127LMerino <127Lmerino@frhsd.com>
2025-09-11 10:12:08 -0400

    REMOVE

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,37 @@
 import java.util.*;
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static ArrayList<String>  list = new ArrayList();
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
         loop();
 
     }
     public static void loop() {
-        System.out.println("?");
+
+        System.out.println("A: SEE TASKS B: ADD TASKS C: REMOVE TASK");
         char prompt = sc.next().charAt(0);
         if (prompt == 'A') {
-
+            view();
         } else if (prompt == 'B') {
 
         } else if (prompt == 'C') {
-
+            remove()
         }
         else{
             System.out.println("FAILED");
             loop();
         }
     }
-    public static void view() {}
+    public static void view() {
+        int i = 0;
+        for (String x : list){
+            System.out.println(i + " " + x);
+            i++;
+        }
+    }
+    public static void remove() {
+        char prompt = sc.nextInt();
+        list.remove(prompt);
+    }
 }
