fd01687fdc6b9330d1c4a227664db5387734cbb1
127LMerino <127Lmerino@frhsd.com>
2025-09-11 10:01:48 -0400

    NEW

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,23 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("?");
+        loop();
+
     }
+    public static void loop() {
+        System.out.println("?");
+        char prompt = sc.next().charAt(0);
+        if (prompt == 'A') {
+
+        } else if (prompt == 'B') {
+
+        } else if (prompt == 'C') {
+
+        }
+        else{
+            System.out.println("FAILED");
+            loop();
+        }
+    }
+    public static void view() {}
 }
